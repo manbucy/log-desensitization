7d8d692d08b3493bc954431abc499311f7d65612
manbucy <1412039603@qq.com>
2022-10-26 23:51:33 +0800

    init

---
+++ log-masking/src/test/java/net/manbucy/log/masking/AppTest.java
@@ -0,0 +1,11 @@
+package net.manbucy.log.masking;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void test_app() {
+        System.out.println("hello log masking");
+    }
+}
