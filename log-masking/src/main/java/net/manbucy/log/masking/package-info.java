a5623a76b17ca0f284716dbff8daf5450fbd9ca8
manbucy <1412039603@qq.com>
2023-02-01 22:55:58 +0800

    simple masking

---
+++ log-masking/src/main/java/net/manbucy/log/masking/package-info.java
@@ -0,0 +1,1 @@
+package net.manbucy.log.masking;
--- log-masking/src/test/java/net/manbucy/log/masking/AppTest.java
+++ log-masking/src/test/java/net/manbucy/log/masking/AppTest.java
@@ -1,11 +1,14 @@
 package net.manbucy.log.masking;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AppTest {
+    private static final Logger log = LoggerFactory.getLogger(AppTest.class);
 
     @Test
     public void test_app() {
-        System.out.println("hello log masking");
+        log.info(LogMaskingUtil.masking("email: 123@test.com, China Mobile: 18212345678, China ID number: 110110199001011010"));
     }
 }
